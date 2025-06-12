3c05824eb28d86702439f1ec0046f7c240682873
Karen Cristine Gonçalves <cris.kgs@gmail.com>
2025-06-12 09:25:13 -0400

    Create 00-prepare_environment.R
    download packages and create new folders

---
+++ scripts/00-prepare_environment.R
@@ -0,0 +1,24 @@
+# Prepare environment by creating right folders
+options(repos=c("https://packagemanager.rstudio.com/all/__linux__/focal/latest",
+                "https://cloud.r-project.org/"))
+
+folders = c("plots", "results", "rdata")
+
+for (folder in folders) dir.create(folder)
+
+# then download the necessary packages 
+
+pkgs=c("devtools", "tidyverse", "igraph", "ggraph",
+       "readxl", "patchwork", "RColorBrewer",
+       "viridis", "parallel", "doParallel",
+       "furrr", "future", "svglite", "BiocManager"
+)
+
+install.packages(pkgs)
+
+BiocManager::install(c("DESeq2", "GenomicFeatures"))
+
+# CustomSelection has to be installed from github, so it cannot be grouped with the others yet
+if (! "CustomSelection" %in% installed.packages() ) {
+  devtools::install_github("KarenGoncalves/CustomSelection")
+}
