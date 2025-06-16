dd036175b7cd5b767b9d337f13aa3d41b98656b7
KarenGoncalves <cris.kgs@gmail.com>
2025-06-16 17:32:03 -0400

    re-run script 04 and prepare PCA metadata

---
+++ scripts/prepare_PCA.sh
@@ -0,0 +1,11 @@
+cut -f 1,2,3,5,10 metadata/metadata.txt |
+ awk -F "\t" 'BEGIN {OFS="\t"; print "Run", "Project", "Layout", "Length_type", "Tissue_group", "Tissue"}
+{
+  if (tolower($5) ~ /flo/ || tolower($5) ~ /bud/) {tissue="Flower"}
+  else if (tolower($5) ~ /meristem/) {tissue="Meristem"}
+  else if (tolower($5) ~ /root/) {tissue="Root"}
+  else if (tolower($5) ~ /stem/ || tolower($5) ~ /shoot/) {tissue="Stem"}
+  else if (tolower($5) ~ /leaf/ || tolower($5) ~ /petiole/) {tissue="Leaf"}
+  else {tissue="Trichome"};
+  print $1, $2, $3, $4, tissue, $5
+} ' > metadata/metadata_pca.txt
